19f451406c28ceed89dc1ee47e1266e959b59570
Filipe Rocha <ancelicus@gmail.com>
2025-04-03 13:23:03 +0100

    Updating index.php

--- index.php
+++ index.php
@@ -3,12 +3,34 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/index.css">
     <title>Document</title>
 </head>
 <body>
     <h1>Paleomapa</h1>
-    <a href="./carregar.php">Carregar dados</a>
-    <a href="./editar.php">Editar dados</a>
-    <a href="./mapa.php">Ver mapa</a>
+
+    <div id="mySidenav" class="sidenav">
+        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+        <a class="loadData" href="./carregar.php">Carregar dados</a>
+        <a class="editData" href="./editar.php">Editar dados</a>
+        <a class="map" href="./mapa.php">Ver mapa</a>    
+    </div>
+
+
+    <span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776;</span>
+    
+    <div id="main">
+
+        <canvas class="mapCanvas">
+        </canvas>
+
+
+
+
+
+    </div>
+
+
+    <script src="./js/index.js"></script>
 </body>
 </html>
